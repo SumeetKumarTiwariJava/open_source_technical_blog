5739a4bcf8becf2c83a2b9020d404ddaee0fa9fe
SumeetKumarTiwariJava <sumeet.tiwari20@gmail.com>
2019-11-16 21:40:35 +0530

    refractoring

--- gcp-pubsub-springboot/src/main/java/com/platform/opensource/controller/HomeController.java
+++ gcp-pubsub-springboot/src/main/java/com/platform/opensource/controller/HomeController.java
@@ -3,6 +3,10 @@ package com.platform.opensource.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * @author sumeet tiwari<OPEN SOURCE TECHNICAL BLOG>
+ *
+ */
 @Controller
 public class HomeController {
 	@RequestMapping("/home")
--- gcp-pubsub-springboot/src/main/java/com/platform/opensource/controller/PubSubController.java
+++ gcp-pubsub-springboot/src/main/java/com/platform/opensource/controller/PubSubController.java
@@ -6,15 +6,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.view.RedirectView;
 
-import com.platform.opensource.GcpPubsubSpringbootApplication.PubsubOutboundGateway;
-
+import com.platform.opensource.igateway.PubsubOutboundGateway;
+/**
+ * @author sumeet tiwari<OPEN SOURCE TECHNICAL BLOG>
+ *
+ */
 @RestController
 public class PubSubController {
 
-	// tag::autowireGateway[]
 	@Autowired
 	private PubsubOutboundGateway messagingGateway;
-	// end::autowireGateway[]
 
 	@PostMapping("/publishMessage")
 	public RedirectView publishMessage(@RequestParam("message") String message) {
