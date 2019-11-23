615b0c5f3ee76454e8c967549a64984c2568400a
SumeetKumarTiwariJava <sumeet.tiwari20@gmail.com>
2019-11-23 12:34:03 +0530

    initial import of gcp-appengine-flexible

---
+++ gcp-appengine-flexible/src/main/java/com/platform/opensource/HelloAppEngine.java
@@ -0,0 +1,30 @@
+package com.platform.opensource;
+
+import java.io.IOException;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author sumeet tiwari<OPEN SOURCE TECHNICAL BLOG>
+ *
+ */
+@WebServlet(
+    name = "HelloAppEngine",
+    urlPatterns = {"/hello"}
+)
+public class HelloAppEngine extends HttpServlet {
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) 
+      throws IOException {
+
+    response.setContentType("text/plain");
+    response.setCharacterEncoding("UTF-8");
+
+    response.getWriter().print("Hello App Engine!\r\n");
+
+  }
+}
